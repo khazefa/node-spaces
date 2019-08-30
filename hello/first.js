dd4ed74b29c0489d78bf3bf7a30448e56b7088bf
Sigit Prayitno <cybergitt@gmail.com>
2019-08-30 23:45:56 +0700

    Update first.js

--- hello/first.js
+++ hello/first.js
@@ -1,12 +1,25 @@
-var http = require("http");
-var additional = require("./modules/Addition.js");
+const http = require("http");
+let additional = require("./modules/Addition.js");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 
+        'Content-Type': 'text/html',
+        "Set-Cookie": "type=ninja"
+    });
     res.write(req.url + "<br>");
     res.write("The additional number are currently: " + additional.AddNumber(10, 13) + "<br>");
     res.write("The additional Date are currently: " + additional.myDateTime());
     res.end();
-}).listen(8081);
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+});
+
+server.listen(port, hostname, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    // Console will print the message
+    console.log('Server running at http://' + hostname + ':'+ port +'/');
+});
