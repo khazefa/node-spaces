8b3f83e4e95a2e9b4b18a9e2d4d759e1428ce840
Sigit Prayitno <cybergitt@gmail.com>
2019-09-05 21:24:00 +0700

    Update Addition.js

--- hello/modules/Addition.js
+++ hello/modules/Addition.js
@@ -1,10 +1,5 @@
 var exports = module.exports = {};
 
-exports.AddNumber = function (a,b) 
-{
-    return a+b;
-};
+exports.AddNumber = (a,b) => a+b;
 
-exports.myDateTime = function () {
-    return Date();
-};
+exports.myDateTime = () => Date();
